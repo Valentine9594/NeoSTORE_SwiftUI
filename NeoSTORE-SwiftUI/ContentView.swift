1ab89664cbab8ef17f23584de204a35fc2c90ffc
Valentine9594 <vtine95@gmail.com>
2022-05-04 12:01:27 +0530

    Initial Commit

---
+++ NeoSTORE-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeoSTORE-SwiftUI
+//
+//  Created by neosoft on 04/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NeoSTORE-SwiftUI/NeoSTORE_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoSTORE_SwiftUIApp.swift
+//  NeoSTORE-SwiftUI
+//
+//  Created by neosoft on 04/05/22.
+//
+
+import SwiftUI
+
+@main
+struct NeoSTORE_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
