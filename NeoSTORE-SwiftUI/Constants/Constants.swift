23db29148985b1a43a82f60a139c398ed87d259c
Valentine9594 <vtine95@gmail.com>
2022-05-04 12:14:14 +0530

    Added MVVM basic file structure of the project.

---
+++ NeoSTORE-SwiftUI/Constants/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  NeoSTORE-SwiftUI
+//
+//  Created by neosoft on 04/05/22.
+//
+
+import Foundation
---
+++ NeoSTORE-SwiftUI/Models/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  NeoSTORE-SwiftUI
+//
+//  Created by neosoft on 04/05/22.
+//
+
+import Foundation
