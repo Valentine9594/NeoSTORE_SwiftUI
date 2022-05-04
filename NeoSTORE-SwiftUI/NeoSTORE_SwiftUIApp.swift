8b73b0b4224eb12f48cc9cb35074b943d3b0ee27
Valentine9594 <vtine95@gmail.com>
2022-05-04 12:22:41 +0530

    Added LoginView file.

--- NeoSTORE-SwiftUI/NeoSTORE_SwiftUIApp.swift
+++ NeoSTORE-SwiftUI/NeoSTORE_SwiftUIApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct NeoSTORE_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
+            LoginView()
         }
     }
 }
---
+++ NeoSTORE-SwiftUI/Views/LoginView/LoginView.swift
@@ -0,0 +1,23 @@
+//
+//  LoginView.swift
+//  NeoSTORE-SwiftUI
+//
+//  Created by neosoft on 04/05/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .background(Color.red)
+            .padding()
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
+
