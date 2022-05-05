4ab94497c48302c4f463bf8be768f5144a6820d6
Valentine9594 <vtine95@gmail.com>
2022-05-05 16:06:16 +0530

    Changed from alignments and padding to spacer.

--- NeoSTORE-SwiftUI/Views/LoginView/LoginView.swift
+++ NeoSTORE-SwiftUI/Views/LoginView/LoginView.swift
@@ -14,72 +14,69 @@ struct LoginView: View {
     var body: some View {
         ZStack(){
             Color.appRed
-            Spacer()
             
-            GeometryReader(content: { geometry in
+            ScrollView(.vertical, showsIndicators: false, content: {
+                VStack(spacing: 0){
+                    Spacer(minLength: 0.14*UIScreen.main.bounds.height)
+                    
+                    Text("NeoSTORE")
+                        .font(.custom("", size: 45))
+                        .foregroundColor(.white)
+                        .bold()
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 49, trailing: 0))
+                    
+                    VStack(alignment: .center, spacing: 16, content: {
+                        TextFieldWithImage(placeholder: Text("Username"), imageName: "username_icon", text: $usernameTextfield)
+                        
+                        SecureFieldWithImage(placeholder: Text("Password"), imageName: "password_icon", text: $passwordTextfield)
+              
+                    })
+                    .padding(EdgeInsets(top: 0, leading: 33, bottom: 0, trailing: 33))
 
-                ScrollView(.vertical, showsIndicators: false, content: {
-                    VStack(spacing: 0){
-                        Spacer(minLength: 0.16*geometry.size.height)
-                        
-                        Text("NeoSTORE")
-                            .font(.custom("", size: 45))
-                            .foregroundColor(.white)
-                            .bold()
-                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 49, trailing: 0))
-                        
-                        VStack(alignment: .center, spacing: 16, content: {
-                            TextFieldWithImage(placeholder: Text("Username"), imageName: "username_icon", text: $usernameTextfield)
-                            
-                            SecureFieldWithImage(placeholder: Text("Password"), imageName: "password_icon", text: $passwordTextfield)
-                  
-                        })
-                        .padding(EdgeInsets(top: 0, leading: 33, bottom: 0, trailing: 33))
-
-                        ButtonWithForegroundAndBackgroundColorWithText(buttonTitle: "LOGIN", buttonForegroundColor: .red, buttonBackgroundColor: .white){
-                            debugPrint("Clicked Login!!!")
-                        }
-                        
-                        Text("Forgot Password?")
-                            .bold()
-                            .foregroundColor(.white)
-                            .padding(.bottom, 33)
-                            .font(.title3)
-                            .multilineTextAlignment(.center)
-                            .onTapGesture {
-                                debugPrint("Tapped Forgot Password!!")
-                            }
-                        
-                        Spacer(minLength: 0.16*geometry.size.height)
-                        
-                        HStack(alignment: .center, spacing: 33, content: {
-                            Text("DON'T HAVE AN ACCOUNT?")
-                                .font(.title3)
-                                .foregroundColor(.white)
-                                .bold()
-                                .frame(width: .none, height: .none, alignment: .leading)
-                            
-                            Image("plus")
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .clipped()
-                                .foregroundColor(.white)
-                                .background(Color.appMaroon)
-                                .frame(width: 46, height: 46, alignment: .trailing)
-                                .onTapGesture {
-                                    debugPrint("Tapped Plus Icon!!")
-                                }
-
-                        })
-                        .frame(maxWidth: .infinity, maxHeight: .infinity)
-                        
+                    ButtonWithForegroundAndBackgroundColorWithText(buttonTitle: "LOGIN", buttonForegroundColor: .red, buttonBackgroundColor: .white){
+                        debugPrint("Clicked Login!!!")
                     }
+                    
+                    Text("Forgot Password?")
+                        .bold()
+                        .foregroundColor(.white)
+                        .padding(.bottom, 33)
+                        .font(.title3)
+                        .multilineTextAlignment(.center)
+                        .onTapGesture {
+                            debugPrint("Tapped Forgot Password!!")
+                        }
+                    
+                    Spacer(minLength: 0.18*UIScreen.main.bounds.height).onAppear(perform: {
+                        debugPrint("space: \(0.18*UIScreen.main.bounds.height)")
+                    })
+                    
+                    HStack(alignment: .center, spacing: 33, content: {
+                        Text("DON'T HAVE AN ACCOUNT?")
+                            .font(.title3)
+                            .foregroundColor(.white)
+                            .bold()
+                            .frame(width: .none, height: .none, alignment: .leading)
+                        
+                        Image("plus")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .clipped()
+                            .foregroundColor(.white)
+                            .background(Color.appMaroon)
+                            .frame(width: 46, height: 46, alignment: .trailing)
+                            .onTapGesture {
+                                debugPrint("Tapped Plus Icon!!")
+                            }
+
+                    })
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     
-                })
+                }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
                 
             })
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
             
         }
         
