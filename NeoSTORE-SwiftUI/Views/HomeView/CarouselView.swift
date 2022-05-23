94f73ef475951e196c56ceb012a879db75e95f26
Valentine9594 <vtine95@gmail.com>
2022-05-23 19:31:15 +0530

    Final commit on virtual mac.

--- NeoSTORE-SwiftUI/Views/HomeView/CarouselView.swift
+++ NeoSTORE-SwiftUI/Views/HomeView/CarouselView.swift
@@ -11,8 +11,17 @@ struct CarouselView: View {
     let carouselImages: [String]
     @Binding var currentPage: Int
     
+    init(carouselImages: [String], currentPage: Binding<Int>){
+        self.carouselImages = carouselImages
+        self._currentPage = currentPage
+        UIPageControl.appearance().currentPageIndicatorTintColor = UIColor(.appRed)
+        UIPageControl.appearance().pageIndicatorTintColor = UIColor(.gray)
+        UIPageControl().subviews.forEach {
+            $0.transform = CGAffineTransform(scaleX: 2, y: 2)
+        }
+    }
+    
     var body: some View {
-        
         TabView(selection: $currentPage){
             ForEach(0..<carouselImages.count, id: \.self) {
                 Image(carouselImages[$0])
--- NeoSTORE-SwiftUI/Views/HomeView/HomeView.swift
+++ NeoSTORE-SwiftUI/Views/HomeView/HomeView.swift
@@ -10,18 +10,10 @@ import SwiftUI
 struct HomeView: View {
     @State private var currentPage: Int = 0
     private let carouselImages = ["slider_img1", "slider_img2", "slider_img3", "slider_img4"]
+    private let homeViewGrid = [GridItem(.flexible(minimum: (UIScreen.main.bounds.width/2)-33), spacing: 12), GridItem(.flexible(minimum: (UIScreen.main.bounds.width/2)-33), spacing: 12)]
     
     var body: some View {
         VStack(spacing: 0){
-//            NavigationView{
-//                HStack{
-//                    Text("NeoSTORE")
-//                        .navigationBarTitleDisplayMode(.inline)
-//                }
-//            }
-//            .background(Color.appRed)
-//            .frame(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height*0.1)
-            
             HStack{
                 Button {
                     debugPrint("Clicked Button")
@@ -37,7 +29,7 @@ struct HomeView: View {
                     .font(.largeTitle)
                     .foregroundColor(.white)
                     .padding()
-                    .frame(height: UIScreen.main.bounds.size.height*0.08)
+                    .frame(height: UIScreen.main.bounds.height*0.08)
                 
                 Spacer()
                 
@@ -47,16 +39,16 @@ struct HomeView: View {
                     Image("search_icon")
                 }
                 .padding(.trailing, 8)
-                .frame(height: UIScreen.main.bounds.size.height*0.08)
+                .frame(height: UIScreen.main.bounds.height*0.08)
             }
-            .frame(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height*0.08)
+            .frame(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.height*0.08)
             .background(Color.appRed)
             
             CarouselView(carouselImages: carouselImages, currentPage: $currentPage)
                 .padding(.bottom, 15)
-                .frame(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height * 0.35)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.35)
             
-            LazyHGrid(rows: [GridItem(.flexible(minimum: (UIScreen.main.bounds.size.width/2)-33), spacing: 12), GridItem(.flexible(minimum: (UIScreen.main.bounds.size.width/2)-33), spacing: 12)], spacing: 12) {
+            LazyHGrid(rows: homeViewGrid, spacing: 12) {
                 ForEach((1...ProductCategories.allCases.count), id: \.self) { index in
                     Image(ProductCategories(rawValue: index)?.imageName ?? "chairsicon")
                         .resizable()
@@ -66,12 +58,13 @@ struct HomeView: View {
                         }
                 }
             }
-            .frame(width: .infinity, height: .infinity)
+            .frame(height: .none)
             .padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 30))
             
             Spacer()
         }
-        .frame(width: .infinity, height: .infinity)
+        .frame(width: UIScreen.main.bounds.width, height: .none)
+        .navigationBarHidden(true)
     }
 }
     
