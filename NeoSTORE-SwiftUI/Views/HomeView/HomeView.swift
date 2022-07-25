8c69eaa70b6fd5b64260761a88f2509825f53283
mayursalvi-ila <mayur.salvi@bank-abc.com>
2022-07-25 18:18:49 +0530

    Commit for ProductListingView initialisation

--- NeoSTORE-SwiftUI/Views/HomeView/HomeView.swift
+++ NeoSTORE-SwiftUI/Views/HomeView/HomeView.swift
@@ -14,35 +14,7 @@ struct HomeView: View {
     
     var body: some View {
         VStack(spacing: 0){
-            HStack{
-                Button {
-                    debugPrint("Clicked Button")
-                } label: {
-                    Image("menu_icon")
-                }
-                .padding(.leading, 8)
-                .frame(height: UIScreen.main.bounds.size.height*0.08)
-                
-                Spacer()
-
-                Text("NeoSTORE")
-                    .font(.largeTitle)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(height: UIScreen.main.bounds.height*0.08)
-                
-                Spacer()
-                
-                Button {
-                    debugPrint("Clicked Button")
-                } label: {
-                    Image("search_icon")
-                }
-                .padding(.trailing, 8)
-                .frame(height: UIScreen.main.bounds.height*0.08)
-            }
-            .frame(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.height*0.08)
-            .background(Color.appRed)
+            NavigationBarView()
             
             CarouselView(carouselImages: carouselImages, currentPage: $currentPage)
                 .padding(.bottom, 15)
---
+++ NeoSTORE-SwiftUI/Views/ProductListingView/ProductListingView.swift
@@ -0,0 +1,23 @@
+//
+//  ProductListingView.swift
+//  NeoSTORE-SwiftUI
+//
+//  Created by Apple on 25/07/22.
+//
+
+import SwiftUI
+
+struct ProductListingView: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            NavigationBarView()
+            
+        }
+    }
+}
+
+struct ProductListingView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductListingView()
+    }
+}
